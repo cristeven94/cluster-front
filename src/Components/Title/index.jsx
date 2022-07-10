fc556533521fe2f1837c1654bc36929c630837ec
Cristian Perafan <cristeven96@gmail.com>
2022-07-10 02:07:27 -0500

    Agrega prop para estilar Title

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { ThemeContext } from "../../Theme";
 import "./index.css";
 
-const Title = ({ text, type = "main" }) => {
+const Title = ({ textClassName, text, type = "main" }) => {
   const { fontColors, fontSizes } = React.useContext(ThemeContext);
   return (
     <div className={`title-container ${type}-type-container h-100 w-100 `}>
-      <div className="title-text-container d-flex f-content-center">
+      <div
+        className={`title-text-container d-flex f-content-center ${
+          textClassName ?? ""
+        }`}
+      >
         <h2 style={{ color: fontColors.primary, fontSize: fontSizes.primary }}>
           {text}
         </h2>
