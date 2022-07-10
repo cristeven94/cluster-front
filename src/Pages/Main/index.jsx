ff7d0681c12cfea3b8b4910a0bfb7c3fbcc3d4d0
Cristian Perafan <cristeven96@gmail.com>
2022-07-09 20:16:39 -0500

    Reubica la página Main

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Login from "../../Login";
+import Login from "../Login";
 
 const Main = () => {
   return (
