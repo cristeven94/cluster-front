26aedcb7f0724c62a8b5928abe9079b7b631de0d
Cristian Perafan <cristeven96@gmail.com>
2022-08-20 21:31:52 -0500

    Adiciona navegación del log al dashboard y detalle

--- src/Components/Form/Login/index.jsx
+++ src/Components/Form/Login/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Formik, useField } from "formik";
 import Button from "../../Button";
 import LoginValidationScheme from "../../../ValidationSchemas/Login";
+import useFetch from "../../../Hooks/useFetch";
 import "./index.css";
+import { Navigate } from "react-router-dom";
 
 const FormField = ({ name, ...rest }) => {
   const [field, meta, helpers] = useField(name);
@@ -22,24 +24,29 @@ const FormField = ({ name, ...rest }) => {
 };
 
 const Form = ({}) => {
+  const [isLoginValid, setIsLoginValid] = React.useState(false);
+  // const { error, loading, response } = useFetch({ endpoint: "all" });
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      validationSchema={LoginValidationScheme}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ handleSubmit }) => (
-        <form className="form d-flex h-100 f-col row-gap-2">
-          <FormField name="username" placeholder="username" />
-          <FormField name="password" placeholder="password" type="password" />
-          <Button
-            className="d-flex f-content-end"
-            onClick={handleSubmit}
-            text="Log in"
-          />
-        </form>
-      )}
-    </Formik>
+    <>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={LoginValidationScheme}
+        onSubmit={(values) => setIsLoginValid(true)}
+      >
+        {({ handleSubmit }) => (
+          <form className="form d-flex h-100 f-col row-gap-2">
+            <FormField name="username" placeholder="username" />
+            <FormField name="password" placeholder="password" type="password" />
+            <Button
+              className="d-flex f-content-end"
+              onClick={handleSubmit}
+              text="Log in"
+            />
+          </form>
+        )}
+      </Formik>
+      {isLoginValid && <Navigate to="/dashboard" />}
+    </>
   );
 };
 
--- src/Pages/ClusterDetail/index.jsx
+++ src/Pages/ClusterDetail/index.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Cluster from "../../Components/Cluster";
 import SideBar from "../../Components/SideBar";
 import Title from "../../Components/Title";
 import { ThemeContext } from "../../Theme";
 import "./index.css";
 
-const ClusterDetail = ({
-  clusterName = "Cluster name",
-  cloudProvider = "Default cloud provider",
-  application = "OpenFaaS",
-}) => {
+const ClusterDetail = ({ clusters = [] }) => {
   const { fontColors, background } = React.useContext(ThemeContext);
+  const { cluster_id } = useParams();
+  const clusterData = clusters.find(
+    (cluster) => cluster.id === Number(cluster_id)
+  );
   return (
     <div className="clusterdetail-page-container d-flex w-100 h-100">
       <SideBar />
       <div className="clusterdetail-content d-flex f-col w-100 h-100">
         <div className="clusterdetail-header">
-          <Title textClassName="f-content-start" text={clusterName} />
+          <Title
+            textClassName="f-content-start"
+            text={clusterData.cluster_name}
+          />
         </div>
         <div
           className="clusterdetail-attributes d-flex f-col"
@@ -28,7 +32,7 @@ const ClusterDetail = ({
               className="attribute-value"
               style={{ backgroundColor: background.terciary }}
             >
-              {cloudProvider}
+              {clusterData.cloud_provider_id.cloud_name}
             </p>
           </div>
           <div className="clusterdetail-attribute d-flex">
@@ -37,12 +41,12 @@ const ClusterDetail = ({
               className="attribute-value"
               style={{ backgroundColor: background.terciary }}
             >
-              {application}
+              {clusterData.application_id.application_name}
             </p>
           </div>
         </div>
         <div className="clusterdetail-body">
-          <Cluster />
+          <Cluster {...clusterData} />
         </div>
       </div>
     </div>
