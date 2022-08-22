a37f61d753f2bb3166e1865796a262617554b254
Cristian Perafan <cristeven96@gmail.com>
2022-08-21 20:05:31 -0500

    Adiciona homepage a package.json

---
+++ src/Endpoints/index.js
@@ -0,0 +1,13 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "crilake01";
+
+export const REQUEST_CREDENTIALS = `${DB_USER}:${DB_PASSWORD}`;
+export const BASE_URL = "http://45.79.180.194:8000/api/";
+export const ENDPOINTS = {
+  ALL: "all/",
+  CLUSTERS: "clusters/",
+  NODES: "nodes/",
+  USERS: "users/",
+  APPLICATIONS: "applications/",
+  CLOUD_PROVIDERS: "cloudProviders/",
+};
