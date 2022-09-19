232a4ffd85b4bbb784e09ef33a859d3a3b792f4f
Cristian Perafan <cristeven96@gmail.com>
2022-09-18 22:16:31 -0500

    Agrega Hook para hacer peticiones

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BASE_URL, REQUEST_CREDENTIALS } from "../Endpoints";
+
+const useFetch = ({
+  endpoint,
+  headers = {
+    Authorization: `Basic ${btoa(REQUEST_CREDENTIALS)}`,
+    "Content-Type": "text/plain",
+  },
+  // method = "GET",
+  // fetchOnClick = false,
+  dependencies = [],
+}) => {
+  // const [clicked, setClicked] = React.useState(false);
+  const [response, setResponse] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    const doFetch = async () => {
+      // if (!fetchOnClick && !clicked) {
+      //   debugger;
+      //   return;
+      // }
+      setLoading(true);
+      console.log([headers, REQUEST_CREDENTIALS]);
+      try {
+        debugger;
+        const res = await fetch(`${BASE_URL}${endpoint}`, {
+          // method,
+          headers,
+        });
+        const json = await res.json();
+        if (!signal.aborted) {
+          setResponse(json);
+        }
+      } catch (err) {
+        if (!signal.aborted) {
+          setError(err);
+        }
+      } finally {
+        if (!signal.aborted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    doFetch();
+    return () => abortController.abort();
+  }, [...dependencies]);
+
+  return { response, error, loading };
+};
+
+export default useFetch;
